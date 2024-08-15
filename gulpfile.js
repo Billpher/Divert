fe95b424733a74595f9a84f0778d731c901d0e95
Lucas <vieiralucas1199@gmail.com>
2024-08-15 16:09:45 -0300

    ajsute do gulp imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('./dist/css'));
+}
+
+function images() {
+    return gulp.src('./src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'))
 }
 
 function scripts() {
@@ -14,9 +21,10 @@ function scripts() {
     .pipe(gulp.dest('./dist/js'))
 }
 
-exports.default = gulp.parallel(styles, scripts);
+exports.default = gulp.parallel(styles, scripts, images);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
+    gulp.watch('./src/images/*', gulp.parallel(images))
 }
